adbc839aeaa044996d8ff886651bd321d0f3b81e
Leonardo Lazilha <107946409+LeonardoLazilha@users.noreply.github.com>
2024-11-10 20:30:06 -0300

    importa json e implementa metodo de filtrar contato pela letra inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { SeparatorComponent } from './components/separator/separator.component';
 import { ContactComponent } from './components/contact/contact.component';
 
+import agenda from './agenda.json';
+
+
+interface Contact {
+  id: number;
+  name: string;
+  phone: string;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -23,4 +32,12 @@ import { ContactComponent } from './components/contact/contact.component';
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contacts: Contact[] = agenda;
+
+  filterContactsByInitialLetter(letter: string): Contact[] {
+    return this.contacts.filter(contact => {
+      return contact.name.toLowerCase().startsWith(letter.toLowerCase());
+    })
+  }
+  
 }
