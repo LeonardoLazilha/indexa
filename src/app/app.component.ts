38a3ecd653a75730beb830aaa0f766cd6fd44969
Leonardo Lazilha <107946409+LeonardoLazilha@users.noreply.github.com>
2024-11-10 14:36:21 -0300

    implementa controle de fluxo, separator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SeparatorComponent } from './components/separator/separator.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ContainerComponent, 
+    HeaderComponent, 
+    SeparatorComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }
